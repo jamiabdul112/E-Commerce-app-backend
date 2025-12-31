2b362c93f92dc7cbc2ea52e64da680235ddabdb4
jamiabdul112 <jamiabdul112@gmail.com>
2025-12-31 22:49:23 +0530

    Change SameSite cookie attribute to 'none'

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -7,7 +7,7 @@ const generateToken = (userId, res)=>{
     res.cookie("jwt", token, {
         maxAge : 15*24*60*1000,
         httpOnly : true,
-        sameSite : "strict",
+        sameSite : "none",
         secure : process.env.NODE_ENV !== "development"
     })
 }
