8876f63a30eb2e4062f80a67be3984ab0039d7e2
jamiabdul112 <jamiabdul112@gmail.com>
2026-01-01 18:18:38 +0530

    Update body parser limits and trust proxy setting
    Increased JSON and URL-encoded body size limits to 20mb.

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,8 @@ cloudinary.config({
 
 const app = express()
 
+app.set("trust proxy", 1);
+
 app.use(
     cors({
         origin: "https://e-commerce-app-frontend-1.onrender.com",
@@ -27,13 +29,11 @@ app.use(
     })
 )
 
-app.use(express.json());
-app.use(cookieParser())
-app.use(express.urlencoded({
-    extended:true
-}))
-app.use(express.json({ limit: "5mb" })); // Allow larger image uploads
-app.use(express.urlencoded({ extended: true, limit: "5mb" }));
+app.use(express.json({ limit: "20mb" })); 
+app.use(express.urlencoded({ extended: true, limit: "20mb" }));
+app.use(cookieParser());
+
+
 
 
 
